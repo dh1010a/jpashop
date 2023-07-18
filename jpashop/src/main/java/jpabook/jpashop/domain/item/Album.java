32254954c80b9573054fddd17f7e0676cbd84ce6
dh1010a <dh1010a@naver.com>
2023-07-18 22:52:38 +0900

    Make Entity_1

---
+++ jpashop/src/main/java/jpabook/jpashop/domain/item/Album.java
@@ -0,0 +1,17 @@
+package jpabook.jpashop.domain.item;
+
+import jakarta.persistence.DiscriminatorValue;
+import jakarta.persistence.Entity;
+import lombok.Getter;
+import lombok.Setter;
+
+@Entity
+@Getter
+@Setter
+@DiscriminatorValue("I")
+public class Album extends Item{
+
+    private String artist;
+
+    private  String etc;
+}
