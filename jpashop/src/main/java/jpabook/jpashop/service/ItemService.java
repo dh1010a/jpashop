5d88e3227c528524e545832384bd859984b80388
dh1010a <dh1010a@naver.com>
2023-07-28 23:55:05 +0900

    fin order_func

--- jpashop/src/main/java/jpabook/jpashop/service/ItemService.java
+++ jpashop/src/main/java/jpabook/jpashop/service/ItemService.java
@@ -1,5 +1,6 @@
 package jpabook.jpashop.service;
 
+import jpabook.jpashop.domain.item.Book;
 import jpabook.jpashop.domain.item.Item;
 import jpabook.jpashop.repository.ItemRepository;
 import lombok.RequiredArgsConstructor;
@@ -18,6 +19,17 @@ public class ItemService {
 	@Transactional
 	public void saveItem(Item item) {
 		itemRepository.save(item);
+	}
+
+	/**
+	 * 영속성 컨텍스트가 자동 변경
+	 */
+	@Transactional
+	public void updateItem(Long id, String name, int price, int stockQuantity) {
+		Item item = itemRepository.findOne(id);
+		item.setName(name);// 가급적 setter 말고 change()메소드 같은거 만들어서 깔끔하게 관리하는게 좋음
+		item.setPrice(price); // 어디서 바뀌는지 추적이 어려운 단점이 있음
+		item.setStockQuantity(stockQuantity);
 	}
 
 	public List<Item> findItems() {
